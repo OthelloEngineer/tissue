18593612eb9f03e8e814deb93d8c68b52017f7c6
OthelloEngineer <jolee18@student.sdu.dk>
2024-04-15 23:24:03 +0200

    added git diff example

--- src/tissuebox_repository.rs
+++ src/tissuebox_repository.rs
@@ -7,6 +7,30 @@
 // Apply difference to github repository. Save ID of tissues to track them on the repository
 //
 //
+//diff --git a/src/integration-test.rs b/src/integration-test.rs
+//index 31d7315..1aee8f7 100644
+//--- a/src/integration-test.rs
+//+++ b/src/integration-test.rs
+//@@ -1,6 +1,9 @@
+// #[cfg(test)]
+// mod tests {
+//-    use crate::{filereader, githandler, todofinder::{self, Submission}};
+//+    use crate::{
+//+        filereader, githandler,
+//+        todofinder::{self, Submission},
+//+    };
+//
+//     use super::*;
+//     use std::fs;
+//@@ -10,7 +13,7 @@ mod tests {
+//             Ok(files) => files,
+//             Err(e) => panic!("Error: {}", e),
+//         };
+//-        let mut submissions : Vec<Submission> = Vec::new();
+//+        let mut submissions: Vec<Submission> = Vec::new();
+//         for file in files {
+//             for line in file.lines {
+//                 let issuer = githandler::blame_user_from_line(&file.file_path, line.0).unwrap();
 
 use async_trait::async_trait;
 use color_eyre::eyre::Result;
